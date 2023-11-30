6ec4d018f25cf1b4bd8ebb4815aacfcb5f4ec747
sabeet <sabeet.other.way@gmail.com>
2023-11-30 01:45:18 -0500

    edited vite config for external build

--- port-moge-3/vite.config.js
+++ port-moge-3/vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"],
-    },
-  },
+  base: "./",
 });
